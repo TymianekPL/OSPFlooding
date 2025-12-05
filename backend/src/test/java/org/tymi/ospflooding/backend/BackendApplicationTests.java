e70bf3533e88eeb221f8e4da3fb4e099a9b66132
TymianekPL <tymi@tymi.org>
2025-12-05 21:21:49 +0100

    Fix the test case to mock the database
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- backend/src/test/java/org/tymi/ospflooding/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/tymi/ospflooding/backend/BackendApplicationTests.java
@@ -1,13 +1,26 @@
 package org.tymi.ospflooding.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.tymi.ospflooding.backend.repositories.RoadSegmentRepository;
 
 @SpringBootTest
+@EnableAutoConfiguration(exclude = {
+        org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration.class,
+        org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration.class
+})
 class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+     @MockitoBean
+     private RoadSegmentRepository roadSegmentRepository;
+
+     @Test
+     void contextLoads() {
+     }
 
 }
