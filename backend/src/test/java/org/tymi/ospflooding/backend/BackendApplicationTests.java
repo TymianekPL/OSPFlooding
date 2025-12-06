6bfc4581dad6e454b9beb2c61122523a96a54046
TymianekPL <tymi@tymi.org>
2025-12-06 12:54:39 +0100

    Fix test suite to include the newly created db repositories
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- backend/src/test/java/org/tymi/ospflooding/backend/BackendApplicationTests.java
+++ backend/src/test/java/org/tymi/ospflooding/backend/BackendApplicationTests.java
@@ -5,19 +5,26 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.tymi.ospflooding.backend.repositories.FloodCacheRepository;
+import org.tymi.ospflooding.backend.repositories.FloodPointRepository;
 import org.tymi.ospflooding.backend.repositories.RoadSegmentRepository;
 
 @SpringBootTest
 @EnableAutoConfiguration(exclude = {
-        org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration.class,
-        org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration.class
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
 })
 class BackendApplicationTests {
 
      @MockitoBean
      private RoadSegmentRepository roadSegmentRepository;
+
+     @MockitoBean
+     private FloodCacheRepository floodCacheRepository;
+
+     @MockitoBean
+     private FloodPointRepository floodPointRepository;
 
      @Test
      void contextLoads() {
