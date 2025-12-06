16c83c849dfba0872207f71a79f4ad5406d5e19b
TymianekPL <tymi@tymi.org>
2025-12-06 20:25:28 +0100

    Properly load .env files
    Signed-off-by: TymianekPL <tymi@tymi.org>

--- backend/src/main/java/org/tymi/ospflooding/backend/BackendApplication.java
+++ backend/src/main/java/org/tymi/ospflooding/backend/BackendApplication.java
@@ -4,16 +4,29 @@ import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 @SpringBootApplication
 public class BackendApplication {
 
      public static void main(String[] args) {
-          Dotenv dotenv = Dotenv.load();
+          Path envPath = Paths.get(".env");
+          if (!Files.exists(envPath)) {
+               envPath = Paths.get("backend/.env");
+          }
 
-          System.setProperty("DB_PORT", dotenv.get("DB_PORT"));
-          System.setProperty("DB_USER", dotenv.get("DB_USER"));
-          System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-          System.setProperty("DB_NAME", dotenv.get("DB_NAME"));
+          Dotenv dotenv = Dotenv.configure()
+                  .filename(envPath.toString())
+                  .load();
+
+          System.setProperty("spring.datasource.url",
+                  "jdbc:mariadb://localhost:" + dotenv.get("DB_PORT") + "/" + dotenv.get("DB_NAME"));
+          System.setProperty("spring.datasource.username", dotenv.get("DB_USER"));
+          System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
+          System.setProperty("spring.datasource.driver-class-name", "org.mariadb.jdbc.Driver");
+
 
           SpringApplication.run(BackendApplication.class, args);
      }
